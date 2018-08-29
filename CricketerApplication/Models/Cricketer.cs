b45c0bc4302983eb12875d859b38e352023a5b66
Ankita Khurana <ankita0096@gmail.com>
2018-08-29 19:30:57 +0530

    Data Annotations

--- CricketerApplication/Models/Cricketer.cs
+++ CricketerApplication/Models/Cricketer.cs
@@ -11,7 +11,9 @@ namespace CricketerApplication.Models
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+
+
     public partial class Cricketer
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
@@ -23,9 +25,20 @@ namespace CricketerApplication.Models
         }
     
         public int ID { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 4)]
         public string Name { get; set; }
+
+        [Required]
+        [Range(1, 500)]
         public Nullable<int> ODI { get; set; }
+
+        [Required]
+        [Range(1, 500)]
         public Nullable<int> Test { get; set; }
+
+        [Required]
         public string Grade { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
